2afb97cd4d7344df15b1e23dcdc8a2b3867798ff
Didier <115608922+DogGotSwag@users.noreply.github.com>
2023-11-02 13:49:36 -0600

    complete singleRound() function
    make user input paramenter case-insensitive
    create results for diffrent varitations with
    appropraite output.

--- script.js
+++ script.js
@@ -18,8 +18,40 @@ function getComputerChoice(){
 }
 
 function singleRound(  playerChoice, computerChoice){
+    playerChoice = playerChoice.toLowerCase();
+    let result = "";
 
+    if(playerChoice == computerChoice){
+        return "It's a tie"
+    }
+
+    else if( playerChoice == "rock" && computerChoice == "paper"){
+        result = "You Lose! Paper beats Rock";
+    }
+
+    else if( playerChoice == "rock" && computerChoice == "scissor"){
+        result = "You Win! Rock beats Scissors";
+    }
+    
+    else if( playerChoice == "paper" && computerChoice == "rock"){
+        result = "You Win! Paper beats Rock";
+    }
+
+    else if( playerChoice == "paper" && computerChoice == "scissor"){
+        result = "You Lose! Scissors beats Paper"
+    }
+
+    else if( playerChoice == "scissor" && computerChoice ==  "rock"){
+        result = "You Lose! Rock beats Scissors";
+    }
+
+    else if( playerChoice == "scissor" && computerChoice == "paper"){
+        result = "You Win! Scissors beats Paper"
+    }
+
+
+    return result;
 }
 
 let playerSelection = "";
-singleRound( playerSelection = prompt("Player Choice") ,getComputerChoice() );
+console.log(singleRound( playerSelection = prompt("Player Choice") ,getComputerChoice() ));
