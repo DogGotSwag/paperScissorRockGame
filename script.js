e7fbad0faeef5d399e0e1ea4a7a8b847781d277d
Didier <115608922+DogGotSwag@users.noreply.github.com>
2023-11-02 13:03:43 -0600

    Add to getComputerChoice()
    now number generated are converted to paper/scissors/rock
    and returned.

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+'use strict';
 function getComputerChoice(){
     let number = Math.floor(Math.random()*3);
-    return number;
+    let computerChoice = "";
+
+    switch(number) {
+        case 0: 
+            computerChoice = "paper";
+            break;
+        case 1:
+            computerChoice = "scissor";
+            break;
+        default:
+            computerChoice = "rock";
+
+    }
+    return computerChoice;
 }
+
 
